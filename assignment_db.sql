491f2ad051403554c4e40ff76e5734cc8fa256d4
ramachandra <ramachandra.t@homelane.com>
2019-06-10 12:43:20 +0530

    removed db sql

--- assignment_db.sql
+++ assignment_db.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 21, 2017 at 10:46 PM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.23
+-- Generation Time: Jun 10, 2019 at 09:12 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ci-login-registration`
+-- Database: `assignment_db`
 --
 
 -- --------------------------------------------------------
@@ -32,18 +32,9 @@ CREATE TABLE `user` (
   `user_email` varchar(50) NOT NULL,
   `user_password` varchar(50) NOT NULL,
   `user_age` int(11) NOT NULL,
-  `user_mobile` int(11) NOT NULL
+  `user_mobile` int(11) NOT NULL,
+  `location` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`user_id`, `user_name`, `user_email`, `user_password`, `user_age`, `user_mobile`) VALUES
-(1, 'Ehtesham', 'ehtesham@gmail.com', '123', 23, 334443333),
-(2, 'Ehtesham', 'ehtesham@gmail.com', '123', 23, 2147483647),
-(3, 'farrukh', 'farrukh@gmail.com', '123', 32, 232342343),
-(4, 'zaid', 'zaid@gmail.com', '202cb962ac59075b964b07152d234b70', 23, 324234234);
 
 --
 -- Indexes for dumped tables
@@ -63,7 +54,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
